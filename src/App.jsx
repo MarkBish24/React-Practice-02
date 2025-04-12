3ce5c60367a40dc111b7009dd5f0f4a2642b09d0
MarkBish24 <markbish24@gmail.com>
2025-04-12 07:03:02 -0700

    completed player section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player inititalName="Player 1" symbol="X" />
+          <Player inititalName="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
